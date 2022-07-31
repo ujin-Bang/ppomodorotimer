804ddb427e51de30d8f725e22e543147d73f8ee3
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-07-31 17:59:20 +0900

    생명주기에 따른 재생설정/ 가독성 명료화 코드작업

--- app/src/main/java/com/restart/ppomodorotimer/MainActivity.kt
+++ app/src/main/java/com/restart/ppomodorotimer/MainActivity.kt
@@ -37,13 +37,31 @@ class MainActivity : AppCompatActivity() {
         initSounds()
     }
 
+    override fun onResume() { //화면에 다시 등장할 때
+        super.onResume()
+
+        soundPool.autoResume() //사운드 풀이 활성화된 모든스트림에 적용
+    }
+
+    override fun onPause() { //생명주기 화면에서 사라졌을때(바탕화면보기 버튼등)
+        super.onPause()
+
+        soundPool.autoPause() //사운드풀이 활성화된 모든스트림에 적용
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        soundPool.release() //앱이 종료되면 사운드풀도 끝낸다. 오디오,비디오 파일은 용량을 많이 차지하기 때문.
+    }
+
     private fun bindView() {
         seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
 
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
 
                 //사용자가 실제로 건드렸을 때만 업데이트 해줌으로써 00초가 일정시간동안 유지되지 않게
-                if (fromUser){
+                if (fromUser) {
                     updateRemainTime(progress * 60 * 1000L)
 
                 }
@@ -63,22 +81,14 @@ class MainActivity : AppCompatActivity() {
                 seekBar
                     ?: return //시크바가 널일 경우 바로 리턴하므로 카운트다운을 진행하지 않게.  좌측에 있는 값이 널일 경우 우측의 값을 리턴한다. 코틀린에서는 익스프레션으로 리턴을 할 수 있기 때문에 온스타트 터치를 바로 리턴하도록.
 
-                //분(progress)를 60을 곱해 초로 만들고 다시 1000을 곱해 밀리세컨드로 만든다. => createCountDownTimer의 인자가 Long타입이므로 Long타입이 들어가야 하기 때문.
-                currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L)
+                startCountDown()
 
-                currentCountDownTimer?.start()
-
-                tickingSoundId?.let{ soundId ->
-
-                    soundPool.play(soundId, 1F, 1F, 0 , -1, 1F)
-
-                }
             }
 
         })
     }
 
-    private fun initSounds(){
+    private fun initSounds() {
 
         tickingSoundId = soundPool.load(this, R.raw.ticking, 1) //사운드풀 로드시 로드된 사운드의 아이디를 반환함.
         bellSoundId = soundPool.load(this, R.raw.bell, 1)
@@ -96,11 +106,37 @@ class MainActivity : AppCompatActivity() {
 
             //타이머 종료시 초기화해주기.
             override fun onFinish() {
-                updateRemainTime(0) //텍스트는 0000으로 초기화
-                updateSeekBar(0) //시크바는 왼쪽으로 초기화
+
+                completeCountDown()
             }
 
         }
+
+    private fun startCountDown(){
+
+        //분(progress)를 60을 곱해 초로 만들고 다시 1000을 곱해 밀리세컨드로 만든다. => createCountDownTimer의 인자가 Long타입이므로 Long타입이 들어가야 하기 때문.
+        currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L)
+
+        currentCountDownTimer?.start()
+
+        tickingSoundId?.let { soundId ->
+
+            soundPool.play(soundId, 1F, 1F, 0, -1, 1F)
+
+        }
+    }
+
+    private fun completeCountDown() {
+        updateRemainTime(0) //텍스트는 0000으로 초기화
+        updateSeekBar(0) //시크바는 왼쪽으로 초기화
+
+        soundPool.autoPause()
+
+        bellSoundId?.let { soundId ->
+
+            soundPool.play(soundId, 1F, 1F, 0, 0, 1F)
+        }
+    }
 
     //들어온 값으로 텍스트뷰에 표현할 분,초값 구하는 함수.
     private fun updateRemainTime(remainMillis: Long) {
